e6827266f5b08440f65c5b44ec5358e9abecfe38
NltdLms <NltdLms@users.noreply.github.com>
2018-01-18 23:45:01 +0530

    Revert "Script error on Name fix"
    This reverts commit c3859edd10c765c8b2bd41a91b236ca26c21fedd.

---
+++ NLTD.EmployeePortal.LMS.Common/DisplayModel/TimeSheetConsolidate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLTD.EmployeePortal.LMS.Common.DisplayModel
+{
+    public class TimeSheetConsolidate
+    {
+        public string name { get; set; }
+        public TimeSpan weeklyHours { get; set; }
+        public TimeSpan monthlyHours { get; set; }
+        public int weeklyPermissionCount { get; set; }
+        public int monthlyPermissionCount { get; set; }
+        public int weeklyLeaveCount { get; set; }
+        public int monthlyLeaveCount { get; set; }
+        public int weeklyLateCount { get; set; }
+        public int monthlyLateCount { get; set; }
+        public int weeklyEarlyCount { get; set; }
+        public int monthlyEarlyCount { get; set; }
+
+        public bool isWeeklyHoursUpdate { get; set; }
+        public bool isMonthlyHoursUpdate { get; set; }
+
+        public string dateRange { get; set; }
+    }
+}
---
+++ NLTD.EmployeePortal.LMS.Ux/Scripts/report.js
@@ -0,0 +1,89 @@
+﻿function loadLateAndEarlyRpt() {
+
+    if ($("#OnlyReportedToMe").val() == undefined) {
+        var showTeam = false;
+    }
+    else {
+        var showTeam = $("#OnlyReportedToMe").prop('checked');
+    }
+    if ($("#IsLeaveOnly").val() == undefined) {
+        var leaveOnly = false;
+    }
+    else {
+        var leaveOnly = $("#IsLeaveOnly").prop('checked');
+    }
+    if ($("#DonotShowRejected").val() == undefined) {
+        var donotshowRejected = false;
+    }
+    else {
+        var donotshowRejected = $("#DonotShowRejected").prop('checked');
+    }
+    if ($("#Name").val() == undefined) {
+        var name = "";
+    }
+    else {
+        if ($("#Name").val() != "") {
+            var name = $("#Name").val().replace(/ /g, "|");
+        }
+        else {
+            name = "";
+        }
+    }
+
+
+    $("#divLoading").show();
+    $("#divForLateandEarlyRpt")
+        .load('/Report/loadLateAndEarlyRpt?Name=' + name + '&FromDate=' + $("#FromDate").val() + '&ToDate=' + $("#ToDate").val() + '&IsLeaveOnly=' + leaveOnly + '&OnlyReportedToMe=' + showTeam + '&reqUsr=' + $("#RequestLevelPerson").val() + '&DonotShowRejected=' + donotshowRejected,
+        function () {
+            $("#LateEarlyrpt").DataTable({ order: [[1, "asc"]]});
+            $("#divLoading").hide();
+            $('html, body').animate({
+                scrollTop: 230  // Means Less header height
+            }, 400);
+        });
+
+}
+
+function LoadNoOfLateReport() {
+
+    if ($("#OnlyReportedToMe").val() == undefined) {
+        var showTeam = false;
+    }
+    else {
+        var showTeam = $("#OnlyReportedToMe").prop('checked');
+    }
+    if ($("#IsLeaveOnly").val() == undefined) {
+        var leaveOnly = false;
+    }
+    else {
+        var leaveOnly = $("#IsLeaveOnly").prop('checked');
+    }
+    if ($("#DonotShowRejected").val() == undefined) {
+        var donotshowRejected = false;
+    }
+    else {
+        var donotshowRejected = $("#DonotShowRejected").prop('checked');
+    }
+    if ($("#Name").val() == undefined) {
+        var name = "";
+    }
+    else {
+        if ($("#Name").val() != "") {
+            var name = $("#Name").val().replace(/ /g, "|");
+        }
+        else {
+            name = "";
+        }
+    }
+
+    $("#divLoading").show();
+    $("#divForNoOfLateRpt")
+        .load('/Report/LoadNoOfLateReport?Name=' + name + '&FromDate=' + $("#FromDate").val() + '&ToDate=' + $("#ToDate").val() + '&IsLeaveOnly=' + leaveOnly + '&OnlyReportedToMe=' + showTeam + '&reqUsr=' + $("#RequestLevelPerson").val() + '&DonotShowRejected=' + donotshowRejected,
+        function () {
+            $("#NoOfLate").DataTable({ order: [[1, "asc"]] });
+            $("#divLoading").hide();
+            $('html, body').animate({
+                scrollTop: 230  // Means Less header height
+            }, 400);
+        });
+}
